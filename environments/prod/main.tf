7fbb27cc7c14a0191aa8327ebe70f06ce1f1084a
Eldho John <eldhomadhurayil@gmail.com>
2026-07-10 00:07:32 +1000

    Add NSG module and subnet Associations

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,10 +1,28 @@
+module "resource_groups" {
+  source = "../../modules/resource-groups"
+
+  location = local.location
+  tags     = local.tags
+}
+
 module "networking" {
   source = "../../modules/networking"
 
-  resource_group_name = "rg-network-prod-aue-001"
+  resource_group_name = module.resource_groups.network_rg_name
   location            = "Australia East"
   vnet_name           = "vnet-hub-prod-aue-001"
   address_space       = ["10.0.0.0/16"]
+
+  tags = local.tags
+}
+
+module "nsg" {
+  source = "../../modules/nsg"
+
+  resource_group_name  = module.resource_groups.network_rg_name
+  location             = local.location
+  management_subnet_id = module.networking.management_subnet_id
+  workload_subnet_id   = module.networking.workload_subnet_id
 
   tags = local.tags
 }
